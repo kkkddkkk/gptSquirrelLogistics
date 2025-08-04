9648b99ec86f4b3770d248c5082148bee2a4d279
yppal29 <yppal29@naver.com>
2025-08-04 22:02:13 +0900

    Footer에서 회원가입시 중단에서부터 시작되는 문제 해결

--- frontend/squirrellogistics/src/App.js
+++ frontend/squirrellogistics/src/App.js
@@ -2,13 +2,17 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
 import RegisterPage from "./pages/Layout/RegisterPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
+    <>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
+    </>
   );
 }
 
